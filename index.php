fb437fc07cc4af07f9466261ce70019f264cfecf
stephencreel <46076458+stephencreel@users.noreply.github.com>
2019-04-09 18:47:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
 
+  <head>
+    <title>Bullio</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+	<link rel="stylesheet" href="styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+  
+  <?php
+		session_start();
+  		include 'navbar.php';
+		include 'authentication.php';
+		include 'modalForms.php';
+		
+	?>
+	
+<div class="jumbotron text-center">
+  <h1>Bullio</h1>
+</div>
+
+<script>
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
+</script>
+
+  
+  </body>
+</html>
